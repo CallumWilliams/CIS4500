af29cc0f1062e64327e8bfdc73c7e2394adc40d0
toxin_4500 <toxin_4500@toxin.local>
2018-02-08 19:12:22 -0500

    Room Desc and ID

--- A2/A2/ViewController.swift
+++ A2/A2/ViewController.swift
@@ -9,18 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    class Decision {
+        
+        var decDescription: String
+        var decDestination: String
+        var decRequirement: String
+        
+        init(desc: String, dest: String, req: String) {
+            
+            self.decDescription = desc
+            self.decDestination = dest
+            self.decRequirement = req
+            
+        }
+        
+    }
+    
+    class Room {
+        
+        var roomID: String
+        var roomDescription: String
+        
+        init(input: String) {
+            
+            //parse initial room data
+            var token = input.components(separatedBy: " ")
+            self.roomID = token[0]
+            
+            let quoteToken = input.components(separatedBy: "\"")
+            self.roomDescription = quoteToken[1]
+            
+            print("|" + input + "|")
+            
+        }
+        
+        func roomInfoOutput() -> String {
+            return self.roomID + ": \"" + self.roomDescription
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        DisplayLabel.numberOfLines = 0
         if let file = Bundle.main.path(forResource: "input", ofType: "txt") {
             do {
                 let contents = try String(contentsOfFile: file)
-                print(contents)
+                let lines = contents.components(separatedBy: .newlines)
+                var roomString = ""
+                var r: [Room] = []
+                print(lines.count)
+                var i = 1
+                for l in lines {
+                    if (l.characters.starts(with: "<") && roomString.count > 0) {
+                        r.append(Room(input: roomString))
+                        roomString = l
+                        i = i + 1
+                    } else {
+                        roomString = roomString + l
+                    }
+                }
+                r.append(Room(input: roomString))
+                
+                for j in 0..<i {
+                    DisplayLabel.text! = DisplayLabel.text! + r[j].roomInfoOutput() + "\n"
+                }
+                
             } catch {
-                print("error")
+                print("Error reading contents")
             }
         } else {
-            print("no")
+            print("ERROR - File not found")
         }
     }
 
